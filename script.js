6672a011fb4ad5a354abd0397c14bf140237b280
Ternamdoo <jerryaliyese@gmail.com>
2026-06-20 23:01:54 +0100

    Create gameboard object
    The gameboard object is the factory for the tic-tac-toe game board.
    The factory:
    - is wrapped in an IIFE to avoid creating any new instance of it.
    - returns a getBoard method that will be used to see the
      current state of the board.
    - will feature new methods later that will be used to interact with the
      game board.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("It works!")
+
+const gameboard = (function Gameboard() {
+  const ROWS = 3;
+  const COLS = 3;
+  const board = [];
+
+  for (let i = 0; i < ROWS; i++) {
+    board[i] = [];
+    for (let j = 0; j < COLS; j++) {
+      board[i].push("");
+    }
+  }
+
+  const getBoard = () => board;
+  
+  return { getBoard }
+})();
